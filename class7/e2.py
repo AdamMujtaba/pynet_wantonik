601bce26142b6741202c2bdafb9e0d0cec1b3c78
Kirk Byers <ktbyers@twb-tech.com>
2016-12-29 17:37:17 -0800

    Improvements to class7, exercise2

--- class7/e2.py
+++ class7/e2.py
@@ -13,13 +13,10 @@ python eapi_vlan.py --remove 100        # remove VLAN100'''
 import pyeapi, argparse
 from pprint import pprint
 
-pynet_sw3 = pyeapi.connect_to('pynet-sw3')
-print pynet_sw3, '\n'
 
-def show_current_vlan_config():
+def show_current_vlan_config(arista_conn):
     # The below section refers to a list, so no .keys, .values, .items methods apply
-    global show_vlan
-    show_vlan = pynet_sw3.enable('show vlan')
+    show_vlan = arista_conn.enable('show vlan')
     print 'What is the data type: ', type(show_vlan)
     print 'How many elements it has: ', len(show_vlan)
     #print show_vlan
@@ -37,26 +34,11 @@ def show_current_vlan_config():
     #print show_vlan
     pprint(show_vlan)
     print 40 * '2', '\n', '\n'
-show_current_vlan_config()
-
-# Argparse section for entering arguments from CLI
-def CLI_arguments():
-    parser = argparse.ArgumentParser(description='Add or remove Vlan depending on the option used')
-    parser.add_argument('--name', help='Configure new Vlan', action='store', dest='vlan_name', type=str)
-    parser.add_argument('--remove', help='Remove the given Vlan ID', action='store_true')
-    parser.add_argument('vlan_id', help='Specify Vlanid i.e. Vlan number', action='store', type=int)
-
-    cli_args = parser.parse_args()
-    global vlan_id
-    vlan_id = cli_args.vlan_id
-    global vlan_name
-    vlan_name = cli_args.vlan_name
-    global remove
-    remove = cli_args.remove
-CLI_arguments()
+    return show_vlan
 
 
-def vlan_config_change():
+def vlan_config_change(arista_conn, vlan_id, vlan_name=""):
+    show_vlan = show_current_vlan_config(arista_conn)
     for vlan in show_vlan.keys():
         if int(vlan) == vlan_id:
             print 80 * '!'
@@ -65,14 +47,34 @@ def vlan_config_change():
             print 'Do you want to remove the existing Vlan? - y/n'
             if raw_input() == 'y':                
                 delete_vlan = ['configure terminal','no vlan ' + str(vlan_id),'name ' + str(vlan_name),'write memory']
-                outp = pynet_sw3.config(delete_vlan)
+                outp = arista_conn.config(delete_vlan)
                 print 'EXECUTING: ', str(outp)
             if raw_input() == 'n':    ## This part doesnt work - program is not exiting.
                 print 'Vlan exists, so nothing to do here'
                 exit()
-
         else:
             add_vlan = ['configure terminal','vlan ' + str(vlan_id),'name ' + str(vlan_name),'write memory']
-            outp = pynet_sw3.run_commands(add_vlan)
+            outp = arista_conn.run_commands(add_vlan)
             print 'EXECUTING: ', str(outp)
-vlan_config_change()
+
+
+def main():
+    # Arista switch definition
+    pynet_sw3 = pyeapi.connect_to('pynet-sw3')
+    print pynet_sw3, '\n'
+
+    parser = argparse.ArgumentParser(description='Add or remove Vlan depending on the option used')
+    parser.add_argument('--name', help='Configure new Vlan', action='store', dest='vlan_name', type=str)
+    parser.add_argument('--remove', help='Remove the given Vlan ID', action='store_true')
+    parser.add_argument('vlan_id', help='Specify Vlanid i.e. Vlan number', action='store', type=int)
+
+    cli_args = parser.parse_args()
+    vlan_id = cli_args.vlan_id
+    vlan_name = cli_args.vlan_name
+    remove = cli_args.remove
+
+    show_current_vlan_config(pynet_sw3)
+    vlan_config_change(pynet_sw3, vlan_id, vlan_name)
+
+if __name__ == "__main__":
+    main()
